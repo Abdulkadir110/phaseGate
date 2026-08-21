095c54d40a55668e1eb3bcfca814cb2ea9b06c9b
Abdulkadir110 <sheuabdulquadir17@gmail.com>
2026-08-21 17:51:50 +0100

    after break

--- dayFive/java/QuestionThree/LongestCommonPrefix.java
+++ dayFive/java/QuestionThree/LongestCommonPrefix.java
@@ -1,10 +1,13 @@
 public class LongestCommonPrefix{
     public static String theCommonPrefix(String[] words){
         String output = "";
+        String firstWord = words.charAt(0);
+        String nextWord = "";
         for(int index = 0; index < words.length; index++){
             for(int secondIndex = 1; secondIndex < words.length; secondIndex++){
-                if(words.charAt(index) == words.charAt(secondIndex)){
-                    output = words.charAt(index);
+                nextWord = words.charAt(secondIndex);
+                if(firstWord.charAt(0) == nextWord.charAt(0) ){
+                    output += firstWord.charAt(0);
                 }
             }
         }
